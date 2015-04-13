47792cea354af7ed9dcfed7678a6c69dea511666
NevesLucas <lneves@bu.edu>
2015-04-13 13:56:43 -0400

    started to implement b&w filter, in progress

--- app/src/main/java/com/example/lucas/cameraapp/Filters.java
+++ app/src/main/java/com/example/lucas/cameraapp/Filters.java
@@ -3,6 +3,7 @@ package com.example.lucas.cameraapp;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.effect.Effect;
 import android.media.effect.EffectContext;
 import android.media.effect.EffectFactory;
@@ -11,13 +12,18 @@ import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ImageView;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 import java.util.Date;
+import java.util.Random;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -34,7 +40,140 @@ public class Filters extends Activity implements GLSurfaceView.Renderer{
     private int mImageWidth;
     private int mImageHeight;
     private boolean mInitialized = false;
+    private volatile boolean saveFrame;
     int mCurrentEffect;
 
+    @Override
+    public void onSurfaceChanged(GL10 gl, int width, int height) {
+        if (mTextureRenderer != null) {
+            mTextureRenderer.updateViewSize(width, height);
+        }
+    }
+
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+    }
+
+    @Override
+    public void onDrawFrame(GL10 gl) {
+        //logic :0000
+        if (!mInitialized) {
+            //Only need to do this once
+            mEffectContext = EffectContext.createWithCurrentGlContext();
+            mTextureRenderer.init();
+            loadTextures();
+            mInitialized = true;
+        }
+        if (mCurrentEffect != R.id.none) {
+            //if an effect is chosen initialize it and apply it to the texture
+            initEffect();
+            applyEffect();
+        }
+        renderResult();
+        if (saveFrame) {
+            saveBitmap(takeScreenshot(gl));
+        }
+    }
+
+    public Bitmap takeScreenshot(GL10 mGL) {
+        final int mWidth = mGLView.getWidth();
+        final int mHeight = mGLView.getHeight();
+        IntBuffer ib = IntBuffer.allocate(mWidth * mHeight);
+        IntBuffer ibt = IntBuffer.allocate(mWidth * mHeight);
+        mGL.glReadPixels(0, 0, mWidth, mHeight, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+
+        // Convert upside down mirror-reversed image to right-side up normal
+        // image.
+        for (int i = 0; i < mHeight; i++) {
+            for (int j = 0; j < mWidth; j++) {
+                ibt.put((mHeight - i - 1) * mWidth + j, ib.get(i * mWidth + j));
+            }
+        }
+
+        Bitmap mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
+        mBitmap.copyPixelsFromBuffer(ibt);
+        return mBitmap;
+    }
+
+    private void saveBitmap(Bitmap bitmap) {
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+            out.flush();
+            out.close();
+            Log.i("TAG", "Image SAVED==========" + file.getAbsolutePath());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void initEffect() {
+        EffectFactory effectFactory = mEffectContext.getFactory();
+        if (mEffect != null) {
+            mEffect.release();
+        }
+        /**
+         * Initialize the correct effect based on the selected menu/action item
+         */
+        if(mCurrentEffect != 0) {
+            //first black and white
+            mEffect = effectFactory.createEffect(
+                    EffectFactory.EFFECT_BLACKWHITE);
+            mEffect.setParameter("black", .1f);
+            mEffect.setParameter("white", .7f);
+        }
+    }
+
+    private void applyEffect() {
+        mEffect.apply(mTextures[0], mImageWidth, mImageHeight, mTextures[1]);
+    }
+
+    private void loadTextures() {
+        // Generate textures
+        GLES20.glGenTextures(2, mTextures, 0);
+
+        // Load input bitmap
+        ImageView myImage = (ImageView) findViewById(R.id.ImageDisplay);
+
+        Bitmap bitmap = ((BitmapDrawable) myImage.getDrawable()).getBitmap();
+/*
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.notpuppy);
+                */
+        mImageWidth = bitmap.getWidth();
+        mImageHeight = bitmap.getHeight();
+        mTextureRenderer.updateTextureSize(mImageWidth, mImageHeight);
+
+        // Upload to texture
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextures[0]);
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+
+        // Set texture parameters
+        GLToolbox.initTexParams();
+    }
+
+    private void renderResult() {
+        if (mCurrentEffect != R.id.none) {
+            // if no effect is chosen, just render the original bitmap
+            mTextureRenderer.renderTexture(mTextures[1]);
+        }
+        else {
+            saveFrame=true;
+            // render the result of applyEffect()
+            mTextureRenderer.renderTexture(mTextures[0]);
+        }
+    }
 
 }
