87bdf60a498ccccbd8e21506813fce3d8d0f1f9b
NevesLucas <lneves@bu.edu>
2015-04-21 00:09:42 -0400

    init update, bugs present

--- app/src/main/java/com/example/lucas/cameraapp/Filters.java
+++ app/src/main/java/com/example/lucas/cameraapp/Filters.java
@@ -2,39 +2,97 @@ package com.example.lucas.cameraapp;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.effect.Effect;
-import android.media.effect.EffectContext;
-import android.media.effect.EffectFactory;
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.GLUtils;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.Menu;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.Date;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
+
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 
 /**
  * Created by Lucas on 4/10/2015.
  */
-public class Filters extends Activity implements GLSurfaceView.Renderer{
-    private GLSurfaceView mGLView;
-    private int[] mTextures = new int[2];
-    private EffectContext mEffectContext;
-    private Effect mEffect;
-    private TextureRenderer mTextureRenderer = new TextureRenderer();
-    private int mImageWidth;
-    private int mImageHeight;
-    private boolean mInitialized = false;
-    int mCurrentEffect;
+public class Filters extends Activity {
+
+
+
+
+    protected Bitmap process(Bitmap original, ColorMatrix colorMatrix) {
+        Bitmap bitmap = Bitmap.createBitmap(
+                original.getWidth(), original.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+        canvas.drawBitmap(original, 0, 0, paint);
+
+        return bitmap;
+    }
+    private ColorMatrix setColorFilter(float R,float G, float B) {
+
+     float sr=(1-R)*.3086f;
+     float sg=(1-G)*.6094f;
+     float sb = (1-B)*.0820f;
+      ColorMatrix Cmat=  new ColorMatrix(new float[]{
+
+                sr+R, sr, sr, 0, 0,
+                sg, sg+G, sg, 0, 50,
+                sb, sb, sb+B,0,0,
+                  0, 0, 0, 0, 1,
+                    0, 0, 0, 0, 1
+        });
+
+        return Cmat;
+    }
+
+
+
+    private ColorMatrix getSepiaColorMatrix() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+
+        ColorMatrix colorScale = new ColorMatrix();
+        colorScale.setScale(1, 1, 0.8f, 1);
+
+        // Convert to grayscale, then apply brown color
+        colorMatrix.postConcat(colorScale);
+
+        return colorMatrix;
+    }
+
+    private ColorMatrix getGrayscaleColorMatrix() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+        return colorMatrix;
+    }
+
+    //blurring effect
+    protected Bitmap blur(android.graphics.Bitmap original, float radius) {
+        Bitmap bitmap = android.graphics.Bitmap.createBitmap(
+                original.getWidth(), original.getHeight(),
+                android.graphics.Bitmap.Config.ARGB_8888);
+
+        RenderScript RenS = RenderScript.create(this);
+
+        Allocation allocIn = Allocation.createFromBitmap(RenS, original);
+        Allocation allocOut = Allocation.createFromBitmap(RenS, bitmap);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(RenS, Element.U8_4(RenS));
+
+        blur.setInput(allocIn);
+        blur.setRadius(radius);
+        blur.forEach(allocOut);
+
+        allocOut.copyTo(bitmap);
+
+        RenS.destroy();
+
+        return bitmap;
+    }
 
 
 }
