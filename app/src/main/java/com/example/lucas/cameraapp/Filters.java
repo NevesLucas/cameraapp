5ebe73a6574b2d7e0df15f5a4994be19afc08c30
NevesLucas <lneves@bu.edu>
2015-04-21 00:38:59 -0400

    filters reworked, added blurring and some color filters, need to link with gui to test

--- app/src/main/java/com/example/lucas/cameraapp/Filters.java
+++ app/src/main/java/com/example/lucas/cameraapp/Filters.java
@@ -2,178 +2,97 @@ package com.example.lucas.cameraapp;
 
 import android.app.Activity;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.media.effect.Effect;
-import android.media.effect.EffectContext;
-import android.media.effect.EffectFactory;
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
-import android.opengl.GLUtils;
-import android.os.Bundle;
-import android.os.Environment;
-import android.util.Log;
-import android.view.Menu;
-import android.widget.ImageView;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.IntBuffer;
-import java.util.Date;
-import java.util.Random;
+import android.graphics.Canvas;
+import android.graphics.ColorMatrix;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
 
 /**
  * Created by Lucas on 4/10/2015.
  */
-public class Filters extends Activity implements GLSurfaceView.Renderer{
-    private GLSurfaceView mGLView;
-    private int[] mTextures = new int[2];
-    private EffectContext mEffectContext;
-    private Effect mEffect;
-    private TextureRenderer mTextureRenderer = new TextureRenderer();
-    private int mImageWidth;
-    private int mImageHeight;
-    private boolean mInitialized = false;
-    private volatile boolean saveFrame;
-    int mCurrentEffect;
+public class Filters extends Activity {
 
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        if (mTextureRenderer != null) {
-            mTextureRenderer.updateViewSize(width, height);
-        }
+
+
+
+    protected Bitmap process(Bitmap original, ColorMatrix colorMatrix) {
+        Bitmap bitmap = Bitmap.createBitmap(
+                original.getWidth(), original.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint paint = new Paint();
+        paint.setColorFilter(new ColorMatrixColorFilter(colorMatrix));
+        canvas.drawBitmap(original, 0, 0, paint);
+
+        return bitmap;
+    }
+    private ColorMatrix setColorFilter(float R,float G, float B) {
+
+        float sr=(1-R)*.3086f;
+        float sg=(1-G)*.6094f;
+        float sb = (1-B)*.0820f;
+        ColorMatrix Cmat=  new ColorMatrix(new float[]{
+
+                sr+R, sr, sr, 0, 0,
+                sg, sg+G, sg, 0, 50,
+                sb, sb, sb+B,0,0,
+                0, 0, 0, 0, 1,
+                0, 0, 0, 0, 1
+        });
+
+        return Cmat;
     }
 
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+
+
+    private ColorMatrix getSepiaColorMatrix() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+
+        ColorMatrix colorScale = new ColorMatrix();
+        colorScale.setScale(1, 1, 0.8f, 1);
+
+        // Convert to grayscale, then apply brown color
+        colorMatrix.postConcat(colorScale);
+
+        return colorMatrix;
     }
 
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        //logic :0000
-        if (!mInitialized) {
-            //Only need to do this once
-            mEffectContext = EffectContext.createWithCurrentGlContext();
-            mTextureRenderer.init();
-            loadTextures();
-            mInitialized = true;
-        }
-        if (mCurrentEffect != R.id.none) {
-            //if an effect is chosen initialize it and apply it to the texture
-            initEffect();
-            applyEffect();
-        }
-        renderResult();
-        if (saveFrame) {
-            saveBitmap(takeScreenshot(gl));
-        }
+    private ColorMatrix getGrayscaleColorMatrix() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        colorMatrix.setSaturation(0);
+        return colorMatrix;
     }
 
-    public Bitmap takeScreenshot(GL10 mGL) {
-        final int mWidth = mGLView.getWidth();
-        final int mHeight = mGLView.getHeight();
-        IntBuffer ib = IntBuffer.allocate(mWidth * mHeight);
-        IntBuffer ibt = IntBuffer.allocate(mWidth * mHeight);
-        mGL.glReadPixels(0, 0, mWidth, mHeight, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+    //blurring effect
+    protected Bitmap blur(android.graphics.Bitmap original, float radius) {
+        Bitmap bitmap = android.graphics.Bitmap.createBitmap(
+                original.getWidth(), original.getHeight(),
+                android.graphics.Bitmap.Config.ARGB_8888);
 
-        // Convert upside down mirror-reversed image to right-side up normal
-        // image.
-        for (int i = 0; i < mHeight; i++) {
-            for (int j = 0; j < mWidth; j++) {
-                ibt.put((mHeight - i - 1) * mWidth + j, ib.get(i * mWidth + j));
-            }
-        }
+        RenderScript RenS = RenderScript.create(this);
 
-        Bitmap mBitmap = Bitmap.createBitmap(mWidth, mHeight, Bitmap.Config.ARGB_8888);
-        mBitmap.copyPixelsFromBuffer(ibt);
-        return mBitmap;
+        Allocation allocIn = Allocation.createFromBitmap(RenS, original);
+        Allocation allocOut = Allocation.createFromBitmap(RenS, bitmap);
+
+        ScriptIntrinsicBlur blur = ScriptIntrinsicBlur.create(RenS, Element.U8_4(RenS));
+
+        blur.setInput(allocIn);
+        blur.setRadius(radius);
+        blur.forEach(allocOut);
+
+        allocOut.copyTo(bitmap);
+
+        RenS.destroy();
+
+        return bitmap;
     }
 
-    private void saveBitmap(Bitmap bitmap) {
-        String root = Environment.getExternalStorageDirectory().toString();
-        File myDir = new File(root + "/saved_images");
-        myDir.mkdirs();
-        Random generator = new Random();
-        int n = 10000;
-        n = generator.nextInt(n);
-        String fname = "Image-"+ n +".jpg";
-        File file = new File (myDir, fname);
-        if (file.exists ()) file.delete ();
-        try {
-            FileOutputStream out = new FileOutputStream(file);
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-            Log.i("TAG", "Image SAVED==========" + file.getAbsolutePath());
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    private void initEffect() {
-        EffectFactory effectFactory = mEffectContext.getFactory();
-        if (mEffect != null) {
-            mEffect.release();
-        }
-        /**
-         * Initialize the correct effect based on the selected menu/action item
-         */
-        if(mCurrentEffect != 0) {
-            //first black and white
-            mEffect = effectFactory.createEffect(
-                    EffectFactory.EFFECT_BLACKWHITE);
-            mEffect.setParameter("black", .1f);
-            mEffect.setParameter("white", .7f);
-        }
-    }
-
-    private void applyEffect() {
-        mEffect.apply(mTextures[0], mImageWidth, mImageHeight, mTextures[1]);
-    }
-
-    private void loadTextures() {
-        // Generate textures
-        GLES20.glGenTextures(2, mTextures, 0);
-
-        // Load input bitmap
-        ImageView myImage = (ImageView) findViewById(R.id.ImageDisplay);
-
-        Bitmap bitmap = ((BitmapDrawable) myImage.getDrawable()).getBitmap();
-/*
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                R.drawable.notpuppy);
-                */
-        mImageWidth = bitmap.getWidth();
-        mImageHeight = bitmap.getHeight();
-        mTextureRenderer.updateTextureSize(mImageWidth, mImageHeight);
-
-        // Upload to texture
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextures[0]);
-        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
-
-        // Set texture parameters
-        GLToolbox.initTexParams();
-    }
-
-    private void renderResult() {
-        if (mCurrentEffect != R.id.none) {
-            // if no effect is chosen, just render the original bitmap
-            mTextureRenderer.renderTexture(mTextures[1]);
-        }
-        else {
-            saveFrame=true;
-            // render the result of applyEffect()
-            mTextureRenderer.renderTexture(mTextures[0]);
-        }
-    }
 
 }
